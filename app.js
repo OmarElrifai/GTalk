fd64a55e0ff08d92052809e8e0198b8ea96bdf11
OmarElrifai <o.erifai40@gmail.com>
2021-09-14 23:59:47 +0200

    second

--- app.js
+++ app.js
@@ -13,19 +13,19 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin","http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header("Access-Control-Allow-Credentials", "true")
-    if (req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
-});
+// app.use((req,res,next)=>{
+//     res.header("Access-Control-Allow-Origin","http://localhost:3000");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//     );
+//     res.header("Access-Control-Allow-Credentials", "true")
+//     if (req.method === "OPTIONS"){
+//         res.header("Access-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 
 
