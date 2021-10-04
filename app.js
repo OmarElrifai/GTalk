5adf70c0172ff7faff577a4fde2321cdd7833883
OmarElrifai <o.erifai40@gmail.com>
2021-10-04 06:14:22 +0200

    Anotheer commit

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(express.json());
 // app.use(express.static('website'));
 app.use("/upload",express.static("upload"));
 app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin","https://gtalk-b4978.web.app/");
+    res.header("Access-Control-Allow-Origin","https://gtalk-b4978.web.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
