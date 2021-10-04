409cffa4744a7b536b6c7c7f9e1bde045858c44e
OmarElrifai <o.erifai40@gmail.com>
2021-10-04 17:06:04 +0200

    commmmm

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const crypto = require("crypto");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
+const cors = require("cors");
 const multer = require("multer");
 let uploadsPhoto =[];
 let pic = {
@@ -17,6 +18,10 @@ let pic = {
 };
 let picture ='';
 let assignurl = false;
+app.use(cors({
+  origin:"https://gtalk-b4978.web.app",
+  credentials:true,
+}))
 const storage = multer.diskStorage({
   destination:(req,file, cb)=>{
     cb(null,__dirname+"/upload/")
@@ -62,19 +67,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 // app.use(express.static('website'));
 app.use("/app/upload",express.static("upload"));
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Origin","https://gtalk-b4978.web.app");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.header("Access-Control-Allow-Credentials", "true")
-    if (req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
-});
+
 
 
 
